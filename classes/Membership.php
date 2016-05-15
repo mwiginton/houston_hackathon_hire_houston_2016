4c13e76ec791cc976e442ac49777136d5309f766
Orlando Gordillo <ogordillo@miners.utep.edu>
2016-05-14 21:34:44 -0500

    updated membership

--- classes/Membership.php
+++ classes/Membership.php
@@ -12,17 +12,17 @@ function validate_user($un, $pwd) {
 	if($ensure_credentials == "superuser")
 	{
 		$_SESSION['status'] = 'authorized';
-		header('location: superuserhome.php');
+		header('location: super-user.php');
 	}
 	if($ensure_credentials == "employer")
 	{
 		$_SESSION['status'] = 'authorized';
-		header('location: nonprofithome.php');
+		header('location: employer-view.php');
 	}
 	if($ensure_credentials == "nonprofit")
 	{
 		$_SESSION['status'] = 'authorized';
-		header('location: nonprofithome.php');
+		header('location: non-profit-view.php');
 	} else return "Please return the correct username and password";
 }
 
