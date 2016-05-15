d102110628044e2a5599e3b0098dd71af1e78802
Orlando Gordillo <ogordillo@miners.utep.edu>
2016-05-14 23:16:10 -0500

    added employer functions

--- scripts/insert_employer.php
+++ scripts/insert_employer.php
@@ -1,7 +1,8 @@
 <?php
-require_once 'classes/Mysql.php';
+require_once '../classes/Mysql.php';
 
 	// variables for input data
+	$user_id = $_POST['user_id'];
 	$user_name = $_POST['user_name'];
 	$password = $_POST['password'];
 	$name = $_POST['name'];
@@ -10,7 +11,19 @@ require_once 'classes/Mysql.php';
 	// sql query for inserting data into database
 	$sql_query = "INSERT INTO appuser(user_name,password) VALUES('$user_name','$password');";
 	$sql_query += "INSERT INTO employer(user_id,name,phone) VALUES(LAST_INSERT_ID(),'$name','$phone');";
-	mysql_query($sql_query);
+//	mysql_query($sql_query);
 
+	$mysqlObj = new Mysql();
+    
+
+
+	if($stmt = $mysqlObj->conn->prepare($sql_query)) {
+		$stmt->execute();
+
+		
+			$stmt->close();
+			
+        
+	}
 
 ?>
