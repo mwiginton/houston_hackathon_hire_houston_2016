631b5124868810f0986228c381bbdf9bde82c8c1
Melissa Martinez <melmar12@live.com>
2016-05-14 21:50:36 -0500

    added about-me paragraph

--- about-us.php
+++ about-us.php
@@ -6,7 +6,7 @@
 
 <div>About Us Page</div>
 
-<p>text</p>
+<p>We are a Houston-based initiative dedicated to identifying job opportunities and helping residents obtain employment. Specifically, our goal is to help place clients of local Non-Profit organizations in jobs where they are more likely to succeed. By doing so, unemployment should decrease, along with recidivism rates in our prisons. The efficacy rate of local Non-Profits should rise, allowing them to increase their impact in the community. Our tool creates a collaborative between employers, job seekers and those who connect gaps between the two. By creating a robust, community-driven job matching platform, _______ removes this burden from local Non-Profits.</p>
 
 </body>
 </html>
