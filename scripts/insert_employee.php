25358fd1cbced4fac74528eedc8d1c16a200d68f
Wiginton <michelle.wiginton00@gmail.com>
2016-05-14 22:38:46 -0500

    modifying insert employee script

--- scripts/insert_employee.php
+++ scripts/insert_employee.php
@@ -13,10 +13,16 @@ require_once 'classes/Mysql.php';
 	$email = $_POST['email'];
 	$start_date = $_POST['start_date'];
 	$end_date = $_POST['end_date'];
+	$tag = $_POST['tag'];
 	 
+	$sql_query = "INSERT INTO employee(organization_user_id,first_name,last_name,phone,address,state,zip,city,email,start_date,end_date) VALUES('$organization_user_id','$first_name','$last_name','$phone','$address','$state','$zip','$city','$email','$start_date','$end_date')";
+	$employee_id = mysql_insert_id();
 	// sql query for inserting data into database
-
-	$sql_query = "INSERT INTO employee(organization_user_id,first_name,last_name,phone,address,state,zip,city,email,start_date,end_date) VALUES('$organization_user_id','$first_name','$last_name','$phone','$address','$state','$zip','$city','$email','$start_date','$end_date');";
+	if(!empty($_POST['tag'])) {
+    	foreach($_POST['tag'] as $i) {
+            $sql_query = "INSERT INTO employee_tag(employee_id,tag_id) VALUES('$employee_id','$+ "i + "')";
+	    }
+	}
  	mysql_query($sql_query);
 
 ?>
