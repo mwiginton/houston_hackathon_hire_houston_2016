c9c6280df0de92373f551448414315eecf6280c9
m1221 <m1221@users.noreply.github.com>
2016-05-14 23:57:38 -0500

    add fonts

--- index-2.php
+++ index-2.php
@@ -8,6 +8,8 @@
 	</script>
 
 <link rel="stylesheet" href="css/awesome-style.css">
+<link href='https://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Oswald:300,400' rel='stylesheet' type='text/css'>
 
 </head>
 <body>
